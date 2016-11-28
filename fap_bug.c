0e631dad6a97520e66ec4a31b2ad40ead788648c
Clement <cheresaz@free.fr>
2016-11-28 16:20:06 +0100

    modif 2

--- fap_bug.c
+++ fap_bug.c
@@ -63,8 +63,11 @@ int est_fap_vide(fap f)
 void
 detruire_fap(fap f)
 {
-	if (f != NULL) {
-      		detruire_fap(f->prochain);
-      		free(f);
+  fap tmp;
+  while (f != NULL)
+    {
+      tmp = f;
+      f = f->prochain;
+      free(tmp);
 	}
 }
